bb0224656cc21a1e9615ded71592ab27614d8042
jungyu <dlwnsrb123@naver.com>
2023-09-09 10:10:14 +0900

    공지추가

---
+++ src/main/java/com/project/notice/file/FileMapper.java
@@ -0,0 +1,33 @@
+package com.project.notice.file;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface FileMapper {
+
+    void saveAll(List<FileDto> files);
+    /**
+     * 파일 리스트 조회
+     */
+    List<FileDto> findAllByNoticeId(Long noticeId);
+
+    /**
+     * 파일 리스트 조회
+     */
+    List<FileDto> findAllByIds(List<Long> ids);
+
+    /**
+     * 파일 삭제
+     * @param ids - PK 리스트
+     */
+    void deleteAllByIds(List<Long> ids);
+    /**
+     * 파일 상세정보 조회
+     * @param id - PK
+     * @return 파일 상세정보
+     */
+    FileDto findById(Long id);
+
+}
